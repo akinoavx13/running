736b911eb2b5b19c2d4e2643f593837a400227a6
Maxime Maheo <maxime@luni.app>
2022-03-06 15:01:05 +0100

    Create project

---
+++ Running/RunningApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningApp.swift
+//  Running
+//
+//  Created by Maxime Maheo on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RunningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
